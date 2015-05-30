6cac69e7cc488ec0d5b3747fc1c083e79e9e1e06
sjdy521 <sjdy521@163.com>
2015-05-30 13:15:37 +0800

    修复好友、群组、消息对象调用dump方法时错误

--- lib/Mojo/Webqq/Base.pm
+++ lib/Mojo/Webqq/Base.pm
@@ -115,10 +115,20 @@ sub reform_hash{
 sub dump{
     my $self = shift;
     my $clone = {};
-    bless $clone,blessed($self) if blessed($self); 
+    my $obj_name = blessed($self);
+    bless $clone,$obj_name if $obj_name; 
     for(keys %$self){
         next if $_ eq "_client";
-        $clone->{$_} = $_ eq "member"?"[ ARRAY ]":$self->{$_};
+        if(my $n=blessed($self->{$_})){
+             $clone->{$_} = "Object($n)";
+        }
+        elsif($_ eq "member" and ref($self->{$_}) eq "ARRAY"){
+            my $member_count = @{$self->{$_}};
+            $clone->{$_} = [ "$member_count of Object(${obj_name}::Member)" ];
+        }
+        else{
+            $clone->{$_} = $self->{$_};
+        }
     }
     print Dumper $clone;
     return $self;
--- lib/Mojo/Webqq/Message/Base.pm
+++ lib/Mojo/Webqq/Message/Base.pm
@@ -1,15 +1,23 @@
 package Mojo::Webqq::Message::Base;
 use Data::Dumper;
-use Storable qw(dclone);
-use List::Util qw(first);
 use Scalar::Util qw(blessed);
 sub dump{
     my $self = shift;
-    my $clone = dclone($self);  
-    for(keys %$clone){
-        if($_ eq "_client"){delete $clone->{$_};next}
-        my $bless_name =  blessed($clone->{$_});
-        $clone->{$_} =  "Object($bless_name)" if defined $bless_name;
+    my $clone = {};
+    my $obj_name = blessed($self);
+    bless $clone,$obj_name if $obj_name;
+    for(keys %$self){
+        next if $_ eq "_client";
+        if(my $n=blessed($self->{$_})){
+             $clone->{$_} = "Object($n)";
+        }
+        elsif($_ eq "member" and ref($self->{$_}) eq "ARRAY"){
+            my $member_count = @{$self->{$_}};
+            $clone->{$_} = [ "$member_count of Object(${obj_name}::Member)" ];
+        }
+        else{
+            $clone->{$_} = $self->{$_};
+        }
     }
     print Dumper $clone;
     return $self;
