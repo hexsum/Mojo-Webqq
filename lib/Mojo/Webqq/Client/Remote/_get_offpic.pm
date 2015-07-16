8982ebb8e3c0b1a8e05490ce2360c9739c7cb46f
sjdy521 <sjdy521@163.com>
2015-07-16 10:03:06 +0800

    receive_offpic事件名称修改为receive_friend_pic 更新相关的文档说明

--- lib/Mojo/Webqq/Client/Remote/_get_offpic.pm
+++ lib/Mojo/Webqq/Client/Remote/_get_offpic.pm
@@ -4,7 +4,7 @@ sub Mojo::Webqq::Client::_get_offpic {
     my $file_path = shift;
     my $friend  = shift;
     
-    return  unless $self->has_subscribers("receive_offpic");
+    return  unless $self->has_subscribers("receive_friend_pic");
     my $api = 'http://w.qq.com/d/channel/get_offpic2';
     my @query_string = (
         file_path   =>  $file_path,
@@ -14,7 +14,7 @@ sub Mojo::Webqq::Client::_get_offpic {
     );
     my $callback = sub{
         my ($data,$ua,$tx) = @_;
-        return  unless $self->has_subscribers("receive_offpic");
+        return  unless $self->has_subscribers("receive_friend_pic");
         return unless defined $data;
         return unless $tx->res->heades->content_type =~/^image\/(.*)/;
         my $type =      $1=~/jpe?g/i        ?   ".jpg"
@@ -35,7 +35,7 @@ sub Mojo::Webqq::Client::_get_offpic {
         close $tmp;
         eval{
             open(my $fh,"<:raw",$tmp->filename) or die $!;
-            $self->emit(receive_offpic => $fh,$tmp->filename,$friend);
+            $self->emit(receive_friend_pic => $fh,$tmp->filename,$friend);
             close $fh;
         };
         $self->error("[Mojo::Webqq::Client::_get_offpic] $@\n") if $@;
