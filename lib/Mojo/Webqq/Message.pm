3a102fce1d976af07beca7dbeba3a97b24753816
sjdy521 <sjdy521@163.com>
2015-12-25 13:18:58 +0800

    2015-12-25 Mojo::Webqq v1.6.8 发布 Merry Christmas!

--- lib/Mojo/Webqq/Message.pm
+++ lib/Mojo/Webqq/Message.pm
@@ -89,7 +89,7 @@ sub gen_message_queue{
             #消息的ttl值减少到0则丢弃消息
             if($msg->ttl <= 0){
                 $self->debug("消息[ " . $msg->msg_id.  " ]已被消息队列丢弃，当前TTL: ". $msg->ttl);
-                my $status = $self->new_send_status(code=>-1,msg=>"发送失败",info=>"TTL失效");
+                my $status = $self->new_send_status(code=>-5,msg=>"发送失败",info=>"TTL失效");
                 if(ref $msg->cb eq 'CODE'){
                     $msg->cb->(
                         $self,
