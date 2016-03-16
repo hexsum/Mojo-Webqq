0d58d894db0a53dfabb3c9d5a6c12befd53ad295
sjdy521 <sjdy521@163.com>
2016-03-16 11:29:47 +0800

    Riddle插件原有api不可用，更新其他api接口 感谢 @Perling 的反馈

--- lib/Mojo/Webqq/Plugin/Riddle.pm
+++ lib/Mojo/Webqq/Plugin/Riddle.pm
@@ -16,27 +16,43 @@ sub call{
         return if ref $data->{ban_user} eq "ARRAY" and first {$_=~/^\d+$/?$msg->sender->qq eq $_:$sender_nick eq $_} @{$data->{ban_user}};
         
         if($flag == 0  and $msg->content eq $command){
-            $client->http_get('http://apis.baidu.com/myml/c1c/c1c',{json=>1,apikey=>$data->{apikey}||'f23168e0956fe11f6fc44ee61dbfa002'},form=>{id=>-1},sub{
-                my $json = shift;
-                return if not defined $json;
-                my $answer = encode("utf8",$json->{Answer});
-                $flag = 1; 
-                $msg->reply("文曲星君题戏三界($json->{id}):\n" . encode("utf8",$json->{Title}),sub{$_[1]->msg_from("bot")}); 
+            $client->steps(
+                sub{
+                    my $delay =shift;
+                    $client->http_get('http://apis.baidu.com/gushi/grid/p1',{json=>1,apikey=>$data->{apikey}||'20d7db97e337ffa35ae0838439c9db5d'},form=>{count=>1,fmt=>0},$delay->begin(0,1));
+                },
+                sub{
+                    my $delay = shift;
+                    my $json = shift;
+                    return if not defined $json;
+                    return if $json->{status} != 0;
+                    my $id = $json->{data}[0]{id} if ref $json->{data} eq "ARRAY";
+                    return if not $id;
+                    $client->http_get('http://apis.baidu.com/gushi/grid/p2',{json=>1,apikey=>$data->{apikey}||'20d7db97e337ffa35ae0838439c9db5d'},form=>{id=>$id,fmt=>0},sub{
+                        my $json = shift;
+                        return if not defined $json;
+                        return if $json->{status} != 0;
+                        return if ref $json->{data} ne "ARRAY";
+                        my $answer = encode("utf8",$json->{data}[0]{body});
+                        $flag = 1; 
+                        $msg->reply("文曲星君题戏三界($json->{data}[0]{id}):\n" . encode("utf8",$json->{data}[0]{title}),sub{$_[1]->msg_from("bot")}); 
 
-                $client->wait(
-                    $data->{timeout} || 30,#等待答案超时时间
-                    sub{#超时公布答案
-                        $flag = 0;
-                        $msg->reply("本题答案：$answer\n偌大的三界之中,难道就没有能懂本星君心意之人么.\n吾独徘徊于天地之间,对酒影成双,知己难求,呜呼哉!");
-                    },
-                    receive_message=>sub{#查看是否有人给出正确答案
-                        my($client,$msg,$timer_id) = @_;
-                        return if $msg->content !~ /\Q$answer\E/;
-                        $flag = 0;
-                        $msg->reply("于千万人之中,文曲星君终于找到了有缘人:\n恭喜 \@" . $msg->sender->displayname . " 回答正确");                      return 1;
-                    }
-                );
-            }); 
+                        $client->wait(
+                            $data->{timeout} || 30,#等待答案超时时间
+                            sub{#超时公布答案
+                                $flag = 0;
+                                $msg->reply("本题答案：$answer\n偌大的三界之中,难道就没有能懂本星君心意之人么.\n吾独徘徊于天地之间,对酒影成双,知己难求,呜呼哉!");
+                            },
+                            receive_message=>sub{#查看是否有人给出正确答案
+                                my($client,$msg,$timer_id) = @_;
+                                return if $msg->content !~ /\Q$answer\E/;
+                                $flag = 0;
+                                $msg->reply("于千万人之中,文曲星君终于找到了有缘人:\n恭喜 \@" . $msg->sender->displayname . " 回答正确");                return 1;
+                            }
+                        );
+                    });
+                },
+            );
         }
     }); 
 }
