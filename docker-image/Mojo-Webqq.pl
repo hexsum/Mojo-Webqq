25207ef10f4706085206a4c1fafc13a31d5affcd
sjdy521 <sjdy521@163.com>
2016-04-27 10:10:38 +0800

    完善docker支持

--- docker-image/Mojo-Webqq.pl
+++ docker-image/Mojo-Webqq.pl
@@ -6,7 +6,14 @@ $host = "0.0.0.0"; #发送消息接口监听地址，修改为自己希望监听
 $port = $ENV{PORT} || 5000;      #发送消息接口监听端口，修改为自己希望监听的端口
 $post_api = $ENV{POST_API};  #接收到的消息上报接口，如果不需要接收消息上报，可以删除或注释此行
 
-my $client = Mojo::Webqq->new(qq=>$qq,log_level=>$ENV{LOG_LEVEL} || "info",ua_debug => $ENV{UA_DEBUG} || 0);
+my $client = Mojo::Webqq->new(
+    qq          =>  $qq,
+    log_encoding=>  $ENV{LOG_ENCODING} || "utf8",
+    log_level   =>  $ENV{LOG_LEVEL} || "info",
+    ua_debug    =>  $ENV{UA_DEBUG} || 0,
+    (defined $ENV{LOG_PATH}?(log_path =>  $ENV{LOG_PATH}):()),
+    (defined $ENV{QRCODE_PATH}?(qrcode_path =>  $ENV{QRCODE_PATH}):()),
+);
 $client->login();
 $client->load("ShowMsg");
 $client->load("Openqq",data=>{listen=>[{host=>$host,port=>$port}], post_api=>$post_api});
