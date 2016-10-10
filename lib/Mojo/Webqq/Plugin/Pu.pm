436c63e66f6f0a553f1e37a1fec0fce2cc8a139d
sjdy521 <sjdy521@163.com>
2016-10-10 15:20:40 +0800

    完善Mojo::Webqq::Plugin::Pu插件的编写规范

--- lib/Mojo/Webqq/Plugin/Pu.pm
+++ lib/Mojo/Webqq/Plugin/Pu.pm
@@ -1,18 +1,21 @@
 package Mojo::Webqq::Plugin::Pu;
 $Mojo::Webqq::Plugin::Pu::PRIORITY = 1;
-
-
-use ZHOUYI::ZhanPu;
-use Encode;
+BEGIN{
+    eval{require ZHOUYI::ZhanPu};
+    our $is_hold_module = 1 unless $@;
+}
+use Mojo::Util qw();
 sub call {
     my $client = shift;
+    $client->die(__PACKAGE__ . '依赖ZHOUYI::ZhanPu模块，请先通过命令"cpanm ZHOUYI::ZhanPu"安装') if !$is_hold_module;
     $client->on(receive_message=>sub{
         my($client,$msg)=@_;
         return if not $msg->allow_plugin;
-        return if $msg->type eq "group_message" and $msg->group->gname eq "Mojolicious";
         return if $msg->content !~ /(周\s*易|占\s*卜|八\s*卦|算\s*命)/;
-        my $reply = Encode::encode("utf8",pu);
-        $client->reply_message($msg,$reply,sub{$_[1]->msg_from("bot")}) if $reply;
+        my $reply;
+        eval{$reply = Mojo::Util::encode("utf8",ZHOUYI::ZhanPu::pu()); };
+        $client->error( __PACKAGE__ . $@);
+        $msg->reply($reply,sub{$_[1]->msg_from("bot")}) if $reply;
     });
 }
 1;
