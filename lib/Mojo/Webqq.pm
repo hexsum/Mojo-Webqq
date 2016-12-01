46c06643bb0f47f139f8591d40488b4d821cb981
sjdy521 <sjdy521@163.com>
2016-12-01 11:52:27 +0800

    2016-12-01 Mojo::Webqq v1.8.9 发布

--- lib/Mojo/Webqq.pm
+++ lib/Mojo/Webqq.pm
@@ -1,7 +1,7 @@
 package Mojo::Webqq;
 use strict;
 use Carp ();
-$Mojo::Webqq::VERSION = "1.8.8";
+$Mojo::Webqq::VERSION = "1.8.9";
 use base qw(Mojo::Base);
 use Mojo::Webqq::Log;
 use Mojo::Webqq::Cache;
