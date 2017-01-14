06754b1069a63ef09ce69419c4a3b67db4a0f10f
Richard Chien <richardchienthebest@gmail.com>
2017-01-14 12:59:09 +0800

    去掉镜像 tag 开头的 "v"

--- .travis_build_docker_image.sh
+++ .travis_build_docker_image.sh
@@ -1,4 +1,5 @@
+export TAG=${TRAVIS_TAG#v}
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker build -f docker-image/Dockerfile -t $DOCKER_REPO docker-image
-docker tag $DOCKER_REPO:latest $DOCKER_REPO:$TRAVIS_TAG
+docker build -f docker-image/Dockerfile -t $DOCKER_REPO:$TAG docker-image
+docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:latest
 docker push $DOCKER_REPO
