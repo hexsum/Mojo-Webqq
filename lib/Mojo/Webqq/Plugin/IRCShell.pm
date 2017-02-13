555e87eca86448a96e12b32a740ecf18005031b1
sjdy521 <sjdy521@163.com>
2017-02-13 15:07:28 +0800

    修复用户更新昵称中存在空格等特殊字符，导致irc虚拟用户异常的问题

--- lib/Mojo/Webqq/Plugin/IRCShell.pm
+++ lib/Mojo/Webqq/Plugin/IRCShell.pm
@@ -470,13 +470,17 @@ sub call{
             return if $property ne "nick" and $property ne "markname";
             my $user = $ircd->search_user(id=>$object->id,virtual=>1);
             return unless defined $user;
-            $user->set_nick($object->displayname) if $object->displayname ne $user->nick;
+            my $displayname = $object->displayname;
+            $displayname=~s/\s|\@|!//g;
+            $user->set_nick($displayname) if $displayname ne $user->nick;
         }
         elsif($object->is_group_member){
             return if $property ne "nick" and $property ne "card"; 
             my $user = $ircd->search_user(id=>$object->id,virtual=>1);
             return unless defined $user;
-            $user->set_nick($object->displayname) if $object->displayname ne $user->nick;
+            my $displayname = $object->displayname;
+            $displayname=~s/\s|\@|!//g;
+            $user->set_nick($displayname) if $displayname ne $user->nick;
         }
         elsif($object->is_group){
             return if $property ne "name";
