5463cf76b1195588145d016c4261c10f65d60dd5
sjdy521 <sjdy521@163.com>
2018-02-14 10:30:29 +0800

    模块检查脚本完善

--- script/check_dependencies.pl
+++ script/check_dependencies.pl
@@ -1,21 +1,40 @@
-my @dependent_modules = qw(
-    Crypt::OpenSSL::RSA 
-    Crypt::OpenSSL::Bignum
-    Compress::Raw::Zlib
-    IO::Compress::Gzip
-    Time::HiRes
-    Time::Piece
-    Time::Seconds
-    Digest::SHA
-    Digest::MD5
-    Encode::Locale
-    IO::Socket::SSL
+my %dependent_modules = (
+    'Crypt::OpenSSL::RSA'       => undef,
+    'Crypt::OpenSSL::Bignum'    => undef,
+    'Compress::Raw::Zlib'       => undef,
+    'IO::Compress::Gzip'        => undef,
+    'Time::HiRes'               => undef,
+    'Time::Piece'               => undef,
+    'Time::Seconds'             => undef,
+    'Digest::SHA'               => undef,
+    'Digest::MD5'               => undef,
+    'Encode::Locale'            => undef,
+    'IO::Socket::SSL'           => undef,
+    'Term::ANSIColor'           => undef,
 );
-print "checking dependencies ...\n";
-print "---------------------\n";
-for my $module (@dependent_modules){
+print "Checking dependencies ...\n";
+print "--------------------------------\n";
+for my $module (keys %dependent_modules){
     eval "require $module";
-    printf "%-30s is %s\n", $module,$@?"not ok":"ok";
+    $dependent_modules{$module} = $@?0:1;
+    printf "%-25s is %s\n", $module,$@?"not ok":"ok";
 }
-print "---------------------\n";
-print "dependencies check over\n";
+print "--------------------------------\n";
+printf "Check result: %d/%d\n",scalar(grep {$dependent_modules{$_}==1} keys %dependent_modules),scalar(keys %dependent_modules);
+if( scalar(grep {$dependent_modules{$_}==0} keys %dependent_modules) == 0){
+    print "Congratulations, all dependencies is ok\n";
+}
+else{
+    print "The below dependence is not found:\n\n";
+    for(grep {$dependent_modules{$_}==0} keys %dependent_modules){
+        print "$_\n";
+    }
+    print "\n";
+    print "You need to install these missing modules by do this command: \n\n";
+    print "    cpanm " . join(" ",grep {$dependent_modules{$_}==0} keys %dependent_modules) . "\n";
+    print "\n";
+
+    print "If you are using Centos, yum is the recommended way which is efficient and reliable:\n\n";
+    print "    yum -y install " . join(" ",map {s/::/-/;"perl-" . $_ } grep {$dependent_modules{$_}==0} keys %dependent_modules) . "\n";
+    print "\n";
+}
